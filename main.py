a14c1422135f60fe3ec38abcda069692758408dd
진수경 <94505794+jinsugyeong@users.noreply.github.com>
2024-01-08 20:51:20 +0900

    Don't worry, be Capy

--- main.py
+++ main.py
@@ -19,9 +19,8 @@ markdown_text = """
         <img src="https://img.shields.io/badge/java-007396?style=flat-square&logo=java&logoColor=white"> 
 	<img src="https://img.shields.io/badge/node.js-339933?style=flat-square&logo=Node.js&logoColor=white"> 
         <img src="https://img.shields.io/badge/PHP-777BB4?style=flat-square&logo=PHP&logoColor=white">
-	<img src="https://img.shields.io/badge/Dart-0175C2?style=flat-square&logo=express&logoColor=white">    
-
-	<img src="https://img.shields.io/badge/html5-E34F26?style=flat-square&logo=html5&logoColor=white"> 
+	<img src="https://img.shields.io/badge/Dart-0175C2?style=flat-square&logo=express&logoColor=white"> 
+	<br><img src="https://img.shields.io/badge/html5-E34F26?style=flat-square&logo=html5&logoColor=white"> 
         <img src="https://img.shields.io/badge/css-1572B6?style=flat-square&logo=css3&logoColor=white">  
         <img src="https://img.shields.io/badge/javascript-F7DF1E?style=flat-square&logo=javascript&logoColor=black"> 
         <img src="https://img.shields.io/badge/jQuery-0769AD?style=flat-square&logo=jQuery&logoColor=white"/> 
